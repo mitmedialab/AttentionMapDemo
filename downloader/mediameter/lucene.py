9f6dcd0fb4f1565e722e23ce417a1917a3be76ed
Rahul Bhargava <rahulb@media.mit.edu>
2013-10-18 21:37:46 -0400

    turning off fake mode

--- downloader/mediameter/lucene.py
+++ downloader/mediameter/lucene.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree
 class Lucene():
 
     SENTENCES_PER_PAGE = 1000
-    FAKE = True
+    FAKE = False
 
     def __init__(self, start_date, end_date, media_id):
         self.log = logging.getLogger('mediameter')
